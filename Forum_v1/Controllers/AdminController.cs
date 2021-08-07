1aad4c23c835aedcecac407fcfffb68c09be1b64
Lutovit <pilot_mig@bk.ru>
2021-08-07 21:17:30 +0300

    fix_01

--- Forum_v1/Controllers/AdminController.cs
+++ Forum_v1/Controllers/AdminController.cs
@@ -18,16 +18,25 @@ namespace Forum_v1.Controllers
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly IGenericRepository<BanEmail> _banRepo;
+        //private readonly IBanEmailRepository _banRepo;
+
+
+        /*
+        public AdminController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, DbContextOptions<ApplicationDbContext> options)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _banRepo = new EFGenericRepository<BanEmail>(new ApplicationDbContext(options));
+        }
+        */
         
-
-
         public AdminController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IGenericRepository<BanEmail> banRepo)
         {
             _userManager = userManager;
             _roleManager = roleManager;
             _banRepo = banRepo;
         }
-
+        
 
 
 
--- Forum_v1/Startup.cs
+++ Forum_v1/Startup.cs
@@ -38,7 +38,9 @@ namespace Forum_v1
 
             services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
-            services.AddTransient< IGenericRepository<BanEmail>, EFGenericRepository<BanEmail> >();
+            services.AddScoped< IGenericRepository<BanEmail>, EFGenericRepository<BanEmail> >();
+
+            //services.AddScoped<IBanEmailRepository, BanEmailRepository>();
         }
 
 
