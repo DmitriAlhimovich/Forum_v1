b33bc3b1b9774eec91978ab19ff0d39241211231
Lutovit <pilot_mig@bk.ru>
2021-08-10 21:20:06 +0300

    tryin to add API

---
+++ Forum.API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+
+        public UsersController(UserManager<ApplicationUser> userManager) 
+        {
+            _userManager = userManager;      
+        }
+
+
+
+        [HttpGet]
+        public IEnumerable<ApplicationUser> Get()
+        {
+            return _userManager.Users;
+        }
+
+
+    }
+}
