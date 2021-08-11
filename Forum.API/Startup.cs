f1bff7ca768b83c72ade24d93a7c61e0652e3a4e
Lutovit <pilot_mig@bk.ru>
2021-08-11 19:48:23 +0300

    added swagger_3

--- Forum.API/Startup.cs
+++ Forum.API/Startup.cs
@@ -49,11 +49,19 @@ namespace Forum.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Forum.API"));
+                
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Forum.API");
+                    c.RoutePrefix = string.Empty;
+                });
             }
 
             app.UseRouting();
