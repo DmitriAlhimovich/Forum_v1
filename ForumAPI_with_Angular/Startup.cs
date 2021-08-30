2cb82bbad86b784c8cf72767424695cf281f95b2
Lutovit <pilot_mig@bk.ru>
2021-08-31 00:27:33 +0300

    trying one more example

--- ForumAPI_with_Angular/Startup.cs
+++ ForumAPI_with_Angular/Startup.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Repository.Absract;
+using Repository.Concrete;
+using Repository.Entities;
 
 namespace ForumAPI_with_Angular
 {
@@ -17,16 +22,32 @@ namespace ForumAPI_with_Angular
 
         public IConfiguration Configuration { get; }
 
+
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddScoped<IGenericRepository<BanEmail>, EFGenericRepository<BanEmail>>();
+
+
+
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
             });
         }
+
+
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
